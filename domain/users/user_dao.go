b6e98e1341b53214189c1af90f2d341992633912
elvin <eremitic.elvin@hotmail.com>
2021-03-02 23:41:57 +0800

    add logger in user services

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,8 +3,8 @@ package users
 import (
 	"fmt"
 	"github.com/eremitic/bookstore_users-api/datasources/mysql/users_db"
+	"github.com/eremitic/bookstore_users-api/logger"
 	"github.com/eremitic/bookstore_users-api/utils/errors"
-	"github.com/eremitic/bookstore_users-api/utils/mysql_utils"
 )
 
 const (
@@ -12,7 +12,7 @@ const (
 	queryUpdate       = "UPDATE users SET first_name=?,last_name=?,email=? where id=?"
 	queryDelete       = "DELETE FROM users where id=?"
 	queryGet          = "SELECT id,first_name,last_name,email,date_created,status from users where id=?"
-	queryFindByStatus = "SELECT id,first_name,last_name,email,date_created,status from users where status=?"
+	queryFindByStatus = "SELEC id,first_name,last_name,email,date_created,status from users where status=?"
 )
 
 func (user *User) Get() *errors.RestErr {
@@ -20,14 +20,17 @@ func (user *User) Get() *errors.RestErr {
 	stmt, err := users_db.Client.Prepare(queryGet)
 
 	if err != nil {
-		return errors.NewInternalErr("user get q err")
+		logger.Error("user get prepare query err", err)
+		return errors.NewInternalErr("db err")
 	}
 
 	defer stmt.Close()
 
 	result := stmt.QueryRow(user.Id)
 	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated, &user.Status); err != nil {
-		return mysql_utils.ParseError(err)
+		logger.Error("user get query err", err)
+		return errors.NewInternalErr("db err")
+
 	}
 
 	return nil
@@ -37,19 +40,23 @@ func (user *User) Save() *errors.RestErr {
 
 	stmt, err := users_db.Client.Prepare(queryInsert)
 	if err != nil {
-		return errors.NewInternalErr("user insert q err")
+		logger.Error("user prepare q query err", err)
+		return errors.NewInternalErr("db err")
 	}
 	defer stmt.Close()
 
 	insRes, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated, user.Password, user.Status)
 
 	if err != nil {
-		return mysql_utils.ParseError(err)
+		logger.Error("user insert q query err", err)
+		return errors.NewInternalErr("db err")
+
 	}
 
 	userId, err := insRes.LastInsertId()
 
 	if err != nil {
+		logger.Error("user save  query err", err)
 		return errors.NewInternalErr("user save err")
 	}
 
@@ -62,19 +69,22 @@ func (user *User) Update() *errors.RestErr {
 
 	stmt, err := users_db.Client.Prepare(queryUpdate)
 	if err != nil {
-		return errors.NewInternalErr("user update q err")
+		logger.Error("user update prepare query err", err)
+		return errors.NewInternalErr("user update q db err")
 	}
 	defer stmt.Close()
 
 	upRes, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.Id)
 
 	if err != nil {
-		return mysql_utils.ParseError(err)
+		logger.Error("user update query err", err)
+		return errors.NewInternalErr("user update q db err")
 	}
 
 	_, err = upRes.RowsAffected()
 
 	if err != nil {
+		logger.Error("user update query row affected err", err)
 		return errors.NewInternalErr("user update err")
 	}
 
@@ -85,14 +95,16 @@ func (user *User) Delete() *errors.RestErr {
 
 	stmt, err := users_db.Client.Prepare(queryDelete)
 	if err != nil {
-		return errors.NewInternalErr("user delete q err")
+		logger.Error("user delete prepare query err", err)
+		return errors.NewInternalErr("user delete prepare q err")
 	}
 	defer stmt.Close()
 
 	upRes, err := stmt.Exec(user.Id)
 
 	if err != nil {
-		return mysql_utils.ParseError(err)
+		logger.Error("user delete query err", err)
+		return errors.NewInternalErr("user delete db")
 	}
 
 	_, err = upRes.RowsAffected()
@@ -108,14 +120,16 @@ func (user *User) FindByStatus(status string) ([]User, *errors.RestErr) {
 	stmt, err := users_db.Client.Prepare(queryFindByStatus)
 
 	if err != nil {
-		return nil, errors.NewInternalErr(err.Error())
+		logger.Error("user find status prepare query err", err)
+		return nil, errors.NewInternalErr("db err")
 	}
 	defer stmt.Close()
 
 	rows, err := stmt.Query(status)
 
 	if err != nil {
-		return nil, errors.NewInternalErr(err.Error())
+		logger.Error("user find status query err", err)
+		return nil, errors.NewInternalErr("db err")
 	}
 
 	defer rows.Close()
@@ -124,7 +138,8 @@ func (user *User) FindByStatus(status string) ([]User, *errors.RestErr) {
 	for rows.Next() {
 		var user User
 		if err := rows.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated, &user.Status); err != nil {
-			return nil, mysql_utils.ParseError(err)
+			logger.Error("user find status parse query err", err)
+			return nil, errors.NewInternalErr("db parse err")
 		}
 		res = append(res, user)
 	}
